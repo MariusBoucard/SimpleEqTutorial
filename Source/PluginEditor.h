4164bd1acea7bd12ca90bf6ff946ef2b2bec6b80
Marius <marius.boucard-bocciarelli@laposte.net>
2023-02-17 20:28:05 +0100

    added les sliders sa mereeeeeee

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -10,6 +10,17 @@
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+//=====================================
+//Custom to create our sliders in the same way and not have to redo every thing again
+
+struct CustomRotatorySlider : juce::Slider
+{
+  CustomRotatorySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+  juce::Slider::TextEntryBoxPosition::NoTextBox)
+  {
+
+  }
+};
 
 //==============================================================================
 /**
@@ -28,6 +39,17 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEqAudioProcessor& audioProcessor;
+    CustomRotatorySlider peakFreqSlider,
+    peakGainSlider,
+    peakQualitySlider,
+    lowCutFreqSlider,
+    highCutFreqSlider,
+    lowCutSlopeSlider,
+    highCutSlopeSlider;
+
+    //Okay we're gonna put them all in a slider so we can iterate over them :
+    std::vector<juce::Component*> getComps();
+
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEqAudioProcessorEditor)
 };
